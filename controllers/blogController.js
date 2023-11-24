d7315d47ef074a2846b1f9cb563ac656ebc501f4
Minh Khoi <nminhkhoi0818@gmail.com>
2023-11-24 08:23:10 +0700

    feat: add pagination and search bar

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,32 +1,42 @@
 const controller = {};
-const sequelize = require('sequelize');
+const sequelize = require("sequelize");
 const models = require("../models");
 
 controller.showList = async (req, res) => {
   const categoryId = req.query.category;
   const tagId = req.query.tag;
+  const page = parseInt(req.query.page) || 1;
+  const searchQuery = req.query.search;
+  const pageSize = 3;
 
   if (!isNaN(categoryId)) {
     res.locals.blogs = await models.Blog.findAll({
-      attributes: ['id', 'title', 'imagePath', 'summary', 'createdAt'],
+      attributes: ["id", "title", "imagePath", "summary", "createdAt"],
       include: [
         { model: models.Comment },
-        { model: models.Category, where: { id: categoryId } }
-      ]
+        { model: models.Category, where: { id: categoryId } },
+      ],
     });
-  }
-  else if (!isNaN(tagId)) {
+  } else if (!isNaN(tagId)) {
     res.locals.blogs = await models.Blog.findAll({
-      attributes: ['id', 'title', 'imagePath', 'summary', 'createdAt'],
+      attributes: ["id", "title", "imagePath", "summary", "createdAt"],
       include: [
         { model: models.Comment },
-        { model: models.Tag, where: { id: tagId } }
-      ]
+        { model: models.Tag, where: { id: tagId } },
+      ],
+    });
+  } else if (searchQuery) {
+    res.locals.blogs = await models.Blog.findAll({
+      attributes: ["id", "title", "imagePath", "summary", "createdAt"],
+      include: [{ model: models.Comment }],
+      where: { title: { [sequelize.Op.iLike]: searchQuery } },
     });
   } else {
     res.locals.blogs = await models.Blog.findAll({
-      attributes: ['id', 'title', 'imagePath', 'summary', 'createdAt'],
-      include: [{ model: models.Comment }]
+      attributes: ["id", "title", "imagePath", "summary", "createdAt"],
+      include: [{ model: models.Comment }],
+      limit: pageSize,
+      offset: (page - 1) * pageSize,
     });
   }
   res.render("index");
@@ -35,28 +45,28 @@ controller.showList = async (req, res) => {
 controller.showDetails = async (req, res) => {
   let id = isNaN(req.params.id) ? 0 : parseInt(req.params.id);
   res.locals.blog = await models.Blog.findOne({
-    attributes: ['id', 'title', 'description', 'createdAt', 'imagePath'],
+    attributes: ["id", "title", "description", "createdAt", "imagePath"],
     where: { id: id },
     include: [
       { model: models.Category },
       { model: models.User },
       { model: models.Tag },
-      { model: models.Comment }
-    ]
+      { model: models.Comment },
+    ],
   });
   res.locals.categoryCounts = await models.Blog.findAll({
     group: ['"Category".id', '"Category"."name"'],
     attributes: [
       '"Category".id',
       '"Category"."name"',
-      [sequelize.fn('COUNT', sequelize.col('"Blog".id')), 'blogCount']
+      [sequelize.fn("COUNT", sequelize.col('"Blog".id')), "blogCount"],
     ],
     include: [
       {
-        attributes: ['name'],
-        model: models.Category
-      }
-    ]
+        attributes: ["name"],
+        model: models.Category,
+      },
+    ],
   });
   res.render("details");
 };
